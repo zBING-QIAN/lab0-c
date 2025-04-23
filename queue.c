4b59d2993f0c83fefae7b158360567b154395f82
zbq.work.ac@gmail.com <zbq.work.ac@gmail.com>
2025-04-24 02:31:10 +0800

    Improve my bottom up q_sort
    I modify my q_sort based on merge sort and make it faster than
    previous version. Unlike recursive calling merge, this implementation
    expand the whole recursive tree according to the binary index.
    
    Change-Id: Ida4c1765083f71e7d45bad80aad0d88e5af13de0

--- queue.c
+++ queue.c
@@ -386,131 +386,46 @@ int q_merge(struct list_head *head, bool descend)
 
 /* Sort elements of queue in ascending/descending order */
 
-static struct list_head *merge(struct list_head *a,
-                               struct list_head *b,
-                               list_cmp_func_t qcmp)
-{
-    struct list_head *head = NULL, **tail = &head;
+/* Sort elements of queue in ascending/descending order */
 
-    for (;;) {
-        /* if equal, take 'a' -- important for sort stability */
-        if (!qcmp(a, b)) {
-            *tail = a;
-            tail = &a->next;
-            a = a->next;
-            if (!a) {
-                *tail = b;
-                break;
-            }
-        } else {
-            *tail = b;
-            tail = &b->next;
-            b = b->next;
-            if (!b) {
-                *tail = a;
-                break;
-            }
-        }
-    }
-    return head;
+int end_bits(int t)
+{
+    int res = 0;
+    for (; t; t >>= 1)
+        if (t & 1)
+            res = (res << 1) | 1;
+    return res;
 }
 
-static void merge_final(struct list_head *head,
-                        struct list_head *a,
-                        struct list_head *b,
-                        list_cmp_func_t qcmp)
-{
-    struct list_head *tail = head;
-    uint8_t count = 0;
-
-    for (;;) {
-        /* if equal, take 'a' -- important for sort stability */
-        if (!qcmp(a, b)) {
-            tail->next = a;
-            a->prev = tail;
-            tail = a;
-            a = a->next;
-            if (!a)
-                break;
-        } else {
-            tail->next = b;
-            b->prev = tail;
-            tail = b;
-            b = b->next;
-            if (!b) {
-                b = a;
-                break;
-            }
-        }
-    }
-
-    /* Finish linking remainder of list b on to tail */
-    tail->next = b;
-    do {
-        if (unlikely(!++count))
-            qcmp(b, b);
-        b->prev = tail;
-        tail = b;
-        b = b->next;
-    } while (b);
-
-    /* And the final links to make a circular doubly-linked list */
-    tail->next = head;
-    head->prev = tail;
-}
-
+// version 2
 void _q_sort(struct list_head *head, list_cmp_func_t qcmp)
 {
-    struct list_head *list = head->next, *pending = NULL;
-    size_t count = 0; /* Count of pending */
-
-    if (list == head->prev) /* Zero or one elements */
+    if (!head || list_empty(head))
         return;
-
-    /* Convert to a null-terminated singly-linked list. */
-    head->prev->next = NULL;
-
-
-    do {
-        size_t bits;
-        struct list_head **tail = &pending;
-
-        /* Find the least-significant clear bit in count */
-        for (bits = count; bits & 1; bits >>= 1)
-            tail = &(*tail)->prev;
-        /* Do the indicated merge */
-        if (likely(bits)) {
-            struct list_head *a = *tail, *b = a->prev;
-
-            a = merge(b, a, qcmp);
-            /* Install the merged result in place of the inputs */
-            a->prev = b->prev;
-            *tail = a;
+    struct list_head *tmp, *l, *r, *cur = head, *nxt = head->next;
+    for (int t = 0; cur->next != head; t++) {
+        nxt = nxt->next;
+        int bits = (nxt == head) ? end_bits(t) : t;
+        for (; bits & 1; bits >>= 1) {
+            r = cur;
+            cur = l = cur->prev;
+            while (l != r && r->next != nxt) {
+                if (qcmp(l->next, r->next)) {
+                    tmp = r->next;
+                    r->next = tmp->next;
+                    tmp->next = l->next;
+                    l->next = tmp;
+                }
+                l = l->next;
+            }
+            nxt->prev = (r->next == nxt) ? r : nxt->prev;
         }
-
-        /* Move one element from input list to pending */
-        list->prev = pending;
-        pending = list;
-        list = list->next;
-        pending->next = NULL;
-        count++;
-    } while (list);
-
-    /* End of input; merge together all the pending lists. */
-    list = pending;
-    pending = pending->prev;
-    for (;;) {
-        struct list_head *next = pending->prev;
-
-        if (!next)
-            break;
-        list = merge(pending, list, qcmp);
-        pending = next;
+        nxt->prev->prev = cur;
+        cur = nxt->prev;
     }
-    /* The final merge, rebuilding prev links */
-    merge_final(head, pending, list, qcmp);
+    for (cur = head->next, cur->prev = head; cur != head; cur = cur->next)
+        cur->next->prev = cur;
 }
-
 
 void q_sort(struct list_head *head, bool descend)
 {
